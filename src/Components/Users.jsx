8d60843529327e4f3452d9a486452c92715c3fa3
nayan-28 <nayanmalakar720@gmail.com>
2024-02-22 01:08:10 +0600

    delete operation completed

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -2,6 +2,18 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
+  const handleDelete = (_id) => {
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("Deleted Successfully");
+          window.location.reload();
+        }
+      });
+  };
   return (
     <div>
       <h2>Users</h2>
@@ -9,9 +21,9 @@ const Users = () => {
         <p key={user._id}>
           <span>Name:</span>
           {user.name}
-          <br></br>
           <span>Email</span>
           {user.email}
+          <button onClick={() => handleDelete(user._id)}>X</button>
         </p>
       ))}
     </div>
